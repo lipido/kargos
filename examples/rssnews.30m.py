60193eebf40f577b9ca8d33afd5871cd18322a98
lipido <dgpena@uvigo.es>
2017-03-29 01:55:26 +0200

    Handles possible exceptions with url connections

--- examples/rssnews.30m.py
+++ examples/rssnews.30m.py
@@ -8,6 +8,9 @@ import xml.etree.ElementTree as ET
 reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('UTF8')
 
+# limit the number of items per url. -1= no limit
+MAX=-1
+
 urls = [
     # (url,icon). icon maybe empty string
     ('https://dot.kde.org/rss.xml', 'https://dot.kde.org/sites/all/themes/neverland/logo.png'),
@@ -15,25 +18,35 @@ urls = [
 ]
 
 
+
 print "---"
-print "Refresh|refresh=true"
+print "Refresh|refresh=true iconName=view-refresh"
 for (url, icon) in urls:
     
     response = urllib2.urlopen(url)
     html = response.read()
 
-
-    root = ET.fromstring(html)
-
-
-    for item in root.findall('.//item'):
-        title = item.find('title').text
-        link = item.find('link').text
-        line = title.replace('|', '/') + '| href=' + link
+    try:
+        root = ET.fromstring(html)
+        count = 0
+        for item in root.findall('.//item'):
+            if count == MAX:
+                    break
+            count += 1
+            title = item.find('title').text
+            link = item.find('link').text
+            line = title.replace('|', '/') + '| href=' + link
+            if (icon !=None and icon != ''):
+                line+=' imageWidth=22 imageHeight=22 imageURL='+icon
+            else:
+                line+=' iconName=application-rss+xml'
+            print line
+            print line + ' dropdown=false'
+    except:
+        line = 'error fetching '+url+'|'
         if (icon !=None and icon != ''):
-            line+=' imageWidth=22 imageURL='+icon
+            line+=' imageWidth=22 imageHeight=22 imageURL='+icon
         else:
             line+=' iconName=application-rss+xml'
         print line
-        print line + ' dropdown=false'
-
+        print line + ' dropdown=false'    
